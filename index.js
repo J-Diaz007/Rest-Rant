f2735ad30c07df7c1029f8958684ec15adbcde52
JDiaz <jdiaz_007@outlook.com>
2023-08-21 18:28:34 -0700

    added 404 page route

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
+//Homepage
 app.get('/', (req, res) => {
     res.send("Sup Y'all")
 })
 
+
+
+//Wildcard route with 404 error (for page that doesn't exist)
+//This should always be at the end of your code!! Or it will override your other pages. :(
+app.get('*', (req, res) => {
+    //Calling .status(404) on the response give a legit 404 code
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+
 app.listen(process.env.PORT)
+
