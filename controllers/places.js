ec9976435aecc47e341aee27c99170b085dedaba
JDiaz <jdiaz_007@outlook.com>
2023-08-24 21:09:26 -0700

    added places view with mock data of kittens

--- controllers/places.js
+++ controllers/places.js
@@ -2,9 +2,23 @@
 //This allows us to keep our index.js file clean & simple
 const router = require('express').Router()
 
+
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]     
+
+    res.render('places/index', { places })
 })
 
 module.exports = router
-
