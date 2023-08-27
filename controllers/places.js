e2dd0857ae6a610a3738afe01f457ea180656db6
JDiaz <jdiaz_007@outlook.com>
2023-08-27 09:51:58 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,10 @@
 //Controller holds the rest of our routes(pages)
 //This allows us to keep our index.js file clean & simple
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 //Index page
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/bakery.jpg'
-      }]     
-
+router.get('/', (req, res) => {   
     res.render('places/index', { places })
 })
 
@@ -28,9 +15,21 @@ router.get('/new', (req, res) => {
 
 
 //Using the POST verb so the data get encrypted for its trip across the internet (safe for passwords and logins)
+//Below code will give a default town and picture if one is not provided
+//res.redirect at the bottom redirects to the index route to see the newly added place
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 
