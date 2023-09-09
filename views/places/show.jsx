47fb4c79ceec8ce72d27b375b4e53f60df48ed1a
JDiaz <jdiaz_007@outlook.com>
2023-09-09 16:59:45 -0700

    populated comment data

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   return (
     <Def>
       <main>
@@ -29,8 +48,7 @@ function show (data) {
           </div>            
         </div>
         <h2>Comments</h2>
-        <div className="row">
-        </div>
+        {comments}
       </main>
     </Def>
   )
@@ -40,4 +58,3 @@ module.exports = show
 
 {/*className btn btn-danger shows the button in red 
 className btn btn-warning shows the button in orange */}
-
